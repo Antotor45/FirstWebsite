c8b0b9cbb5a001677a02c533a865d95e4d9234be
Antonin <antonindosouto@gmail.com>
2023-01-02 00:26:57 +0100

    Modification

--- src/main/java/fr/avainfo/firstwebsite/Test.java
+++ src/main/java/fr/avainfo/firstwebsite/Test.java
@@ -3,12 +3,18 @@ package fr.avainfo.firstwebsite;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Test {
     @GetMapping("/")
     public String index(Model model) {
-
         return "Test";
+    }
+
+    @GetMapping("/model")
+     public String model(@RequestParam(defaultValue = "Default", required = false, name = "value") String params, Model model) {
+        model.addAttribute("value", params);
+        return "ModelEdit";
     }
 }
